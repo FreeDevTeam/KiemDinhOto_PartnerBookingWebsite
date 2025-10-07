e5dee8df8476b82d13368753a7945e3438039b4b
sang901-dev <216698415+sang901-dev@users.noreply.github.com>
2025-10-07 17:23:32 +0700

    Check thêm điều kiện tự gán stationsId

--- src/page/BookingPartner/bookingPartnerForm.js
+++ src/page/BookingPartner/bookingPartnerForm.js
@@ -498,7 +498,9 @@ function BookingPartnerForm({ form, setTabKey, zaloUserName, zaloUserPhone }) {
           const selectedStation = priorityStation || activeStations[0]
           setStationSelected(selectedStation?.stationsId)
           setWorkdaySelectedDate(undefined)
-          if (dataBookingParam?.stationsId) {
+
+          const hasStationIdByConfig = stationList?.find((item) => item?.stationsId === dataBookingParam?.stationsId)
+          if (dataBookingParam?.stationsId && hasStationIdByConfig) {
             setStationSelected(dataBookingParam?.stationsId)
             form.setFieldValue('stationsId', dataBookingParam?.stationsId)
           } else {
