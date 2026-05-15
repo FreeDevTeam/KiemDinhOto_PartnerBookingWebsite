c95183c45d08d2e109f5bd5cf683193a492a9ed1
Hien Luong <nguyenhienluong200212@gmail.com>
2026-05-15 18:53:46 +0700

    Thêm trang bảo trì

--- src/router.js
+++ src/router.js
@@ -17,6 +17,8 @@ const PersonalGuide = React.lazy(() => import('./page/Guide/personalGuide'))
 const UpdateBookingDetail = React.lazy(() => import('./page/BookingDetail/UpdateBookingDetail'))
 const IframeView = React.lazy(() => import('./page/IframeView'))
 const TaxLookupMst = React.lazy(() => import('./page/TaxCDS/TaxLookupMst'))
+const MaintenancePage = React.lazy(() => import('./page/Maintenance/index'));
+
 const BookingType=process.env.REACT_APP_BHTNDS
 const DEFAULT_HOME_LAYOUT=process.env.REACT_APP_DEFAULT_HOME_LAYOUT
 
@@ -91,6 +93,12 @@ export const ROUTERS = {
         component: TaxLookupMst,
         isZaloApp: 1
     },
+    maintenance: {
+        path: '/maintenance',
+        component: MaintenancePage,
+        isZaloApp: 1 // nếu muốn cho Zalo app
+    },
+
     // page on web
     homePageWeb: {
         path: PATH.HOME,
@@ -146,6 +154,11 @@ export const ROUTERS = {
         path: PATH.TAX_LOOKUP_MST,
         component: TaxLookupMst,
         isZaloApp: 0
+    },
+    maintenanceWeb: {
+        path: '/maintenance',
+        component: MaintenancePage,
+        isZaloApp: 0 // nếu muốn cho web
     },
     // bookingPartnerIframe: {
     //     path: PATH.BOOKING_PARTNER_IFRAME,
